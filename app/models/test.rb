7a18c245fbb96bf55cab15f683b8e342f6a61425
Serge <s@shayderov.ru>
2019-08-31 17:02:32 +0300

    Add dependent options for has_many associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 class Test < ApplicationRecord
-  has_many :questions, dependent: :destroy
-  has_many :attempts
-  has_many :users, through: :attempts
   belongs_to :category
   belongs_to :author, class_name: "User", foreign_key: "author_id"
+  has_many :questions, dependent: :destroy
+  has_many :attempts, dependent: :destroy
+  has_many :users, through: :attempts
 
   def self.of_category(category_title)
     joins(:category)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_193736) do
+ActiveRecord::Schema.define(version: 2019_08_31_135510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,10 +52,10 @@ ActiveRecord::Schema.define(version: 2019_08_30_193736) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0
-    t.integer "category_id", null: false
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
