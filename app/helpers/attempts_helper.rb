11e4c5d7753ff60c49821306e2a62ee57dc0deae
Serge <s@shayderov.ru>
2019-11-19 20:49:40 +0300

    Refactor attempt result helper

--- app/helpers/attempts_helper.rb
+++ app/helpers/attempts_helper.rb
@@ -2,13 +2,12 @@ module AttemptsHelper
   PASSING_SCORE = 85
 
   def result(attempt)
-    pass = attempt.successful? ? "passed" : "failed"
-    p_class = { "passed" => "text-success", "failed" => "text-danger" }
-    content_tag(
-      :p,
-      "#{attempt.score}% - #{t('.' + pass)}",
-      class: p_class[pass]
-    )
+    pass, html_class = if attempt.successful?
+                         [".passed", "text-success"]
+                       else
+                         [".failed", "text-danger"]
+                       end
+    content_tag :p, "#{attempt.score}% - #{t(pass)}", class: html_class
   end
 
   def progress(attempt)
