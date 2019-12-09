d8217505b3e2d21ded1140b9fe965718f7042650
Serge <s@shayderov.ru>
2019-12-09 19:02:39 +0300

    Badge helpers localization

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,2 +1,10 @@
 module BadgesHelper
+  def condition_select(badge)
+    select(
+      badge,
+      :condition,
+      Badge.conditions.keys.map { |c| [t(".#{c}"), c] },
+      { prompt: true, class: "form-control" }
+    )
+  end
 end
